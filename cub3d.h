d37be55ec94af95c76c23b859fd763188634fe62
Inigo Alvarez echarri <ialvarez@c2r18s3.42madrid.com>
2023-09-27 14:55:12 +0200

    cub

---
+++ cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialvarez <ialvarez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 14:53:49 by ialvarez          #+#    #+#             */
+/*   Updated: 2023/09/27 14:54:26 by ialvarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libft/libft.h"
+# include "key.h"
+# include <mlx.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <math.h>
+
+#endif
