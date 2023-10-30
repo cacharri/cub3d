0cdff31210f86d322e1bf63b49a9e0d51530a209
dabel-co <dabel-co@student.42madrid.com>
2023-10-30 16:36:07 +0100

    background okay

--- src/cube.c
+++ src/cube.c
@@ -12,7 +12,46 @@
 
 #include "cub3d.h"
 
+void	my_mlx_pixel_put(t_img *img, int x, int y, long color)
+{
+	char	*dst;
 
+	dst = img->addr + (y * img->len + x
+			* (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	draw_background(t_img bg, int ceiling, int floor)
+{
+	int	x;
+	int	y;
+	int	tex;
+
+	y = 0;
+	tex = ceiling;
+	while (y < HEIGHT)
+	{
+		if (y == HEIGHT / 2)
+			tex = floor;
+		x = 0;
+		while (x < WIDTH)
+		{
+			my_mlx_pixel_put(&bg, x, y, tex);
+			x++;
+		}
+		y++;
+	}
+}
+
+int	game(t_game *game)
+{
+	mlx_clear_window(game->ptr, game->win);
+	//check_movement(g);
+	draw_background(game->bg, game->ceiling, game->floor);
+	//raycast(g);
+	mlx_put_image_to_window(game->ptr, game->win, game->bg.img, 0, 0);
+	return(0);	
+}
 static void	load_img(t_game *x, t_info *data)
 {
 	x->tex.n.img = mlx_xpm_file_to_image(x->ptr, data->s,
@@ -43,6 +82,11 @@ void	init_cub(t_game *init, t_info *data)
 {	
 	init->ptr = mlx_init();
 	init->win = mlx_new_window(init->ptr, WIDTH, HEIGHT, "cub3d");
-	//mlx_loop(init->ptr);
 	load_img(init, data);
+	init->map = copy_matrix(data->map);
+	init->floor = data->floor;
+	init->ceiling = data->ceiling;
+	free_info(data);
+	mlx_loop_hook(init->ptr, game, init);
+	mlx_loop(init->ptr);
 }
